9b39493006e7debbada729465ab4c87f34f43d03
o0pmitev <pmitev89@gmail.com>
2020-04-30 12:25:08 +0200

    Finished

---
+++ script.js
@@ -0,0 +1,59 @@
+// openweathermap API
+const key = 'fea7d518190229011784764d73404219';
+
+
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const weatherContainer = document.querySelector('.weather-container');
+
+form.addEventListener('submit', function() {
+  event.preventDefault();
+  let city = input.value;
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${key}`
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+
+    let temp = Math.round(data.main.temp - 273);
+    let weatherDescription = data.weather[0].description;
+    let icon = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let backgroundColor;
+
+    // Pick color for the background
+    if(temp > 40) {
+      backgroundColor = 'red';
+    } else if( temp > 35) {
+      backgroundColor = 'orange';
+    } else if( temp > 30) {
+      backgroundColor = 'yellow';
+    } else if(temp > 20) {
+      backgroundColor = 'deepskyblue'
+    } else if(temp > 10) {
+      backgroundColor = 'purple'
+    } else if (temp === 0) {
+      backgroundColor = 'grey'
+    } else if (temp < 0) {
+      backgroundColor = 'white'
+    }
+    
+    console.log(backgroundColor)
+    let weatherCard = 
+    ` 
+    <div class="weather-card">     
+    <p class="city-name">${city}</p>
+    <p>${temp} &#176C</p>
+    <p>${weatherDescription}<img src="${icon}"></p>
+    </div>
+    `;
+
+    weatherContainer.innerHTML = weatherCard;
+
+    const weatherCardBackground  = document.querySelector('.weather-card');
+    weatherCardBackground.style.backgroundColor = backgroundColor;
+
+    console.log(data.weather[0].icon);
+
+  })
+});
+
+
